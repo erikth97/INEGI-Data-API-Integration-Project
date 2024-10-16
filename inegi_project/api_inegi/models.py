d0395273f8c765bdb349c3941a6eb51d17ae2fe1
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-10-16 01:05:27 -0600

    feat: Setup models for State, Municipality, Locality, and Settlement, and initial project configuration

--- inegi_project/api_inegi/models.py
+++ inegi_project/api_inegi/models.py
@@ -1,51 +1,43 @@
 from django.db import models
 
-# Modelo para los Estados
 class Estado(models.Model):
+    cve_ent = models.CharField(max_length=2, unique=True, default='00')  # Proporciona un valor por defecto
     nombre = models.CharField(max_length=100)
-
-    class Meta:
-        verbose_name = "Estado"
-        verbose_name_plural = "Estados"
+    nombre_abrev = models.CharField(max_length=10, blank=True, null=True)
+    pob_total = models.CharField(max_length=10, blank=True, null=True)
+    pob_femenina = models.CharField(max_length=10, blank=True, null=True)
+    pob_masculina = models.CharField(max_length=10, blank=True, null=True)
+    total_viviendas_habitadas = models.CharField(max_length=10, blank=True, null=True)
 
     def __str__(self):
         return self.nombre
 
-
-# Modelo para los Municipios
 class Municipio(models.Model):
+    cve_mun = models.CharField(max_length=4, default='0000')  # Proporciona un valor por defecto
     nombre = models.CharField(max_length=100)
     estado = models.ForeignKey(Estado, related_name='municipios', on_delete=models.CASCADE)
 
-    class Meta:
-        verbose_name = "Municipio"
-        verbose_name_plural = "Municipios"
-
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} - {self.estado.nombre}"
 
-
-# Modelo para las Localidades
 class Localidad(models.Model):
+    cve_loc = models.CharField(max_length=4, default='0000')  # Proporciona un valor por defecto
     nombre = models.CharField(max_length=100)
     municipio = models.ForeignKey(Municipio, related_name='localidades', on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = "Localidad"
-        verbose_name_plural = "Localidades"
+    latitud = models.CharField(max_length=15, blank=True, null=True)
+    longitud = models.CharField(max_length=15, blank=True, null=True)
+    altitud = models.CharField(max_length=10, blank=True, null=True)
+    pob_total = models.CharField(max_length=10, blank=True, null=True)
+    total_viviendas_habitadas = models.CharField(max_length=10, blank=True, null=True)
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} - {self.municipio.nombre}"
 
-
-# Modelo para los Asentamientos
 class Asentamiento(models.Model):
-    nombre = models.CharField(max_length=100)
+    cve_asen = models.CharField(max_length=4, default='0000')  # Proporciona un valor por defecto
+    nombre = models.CharField(max_length=255)
+    tipo_asen = models.CharField(max_length=50, blank=True, null=True)
     localidad = models.ForeignKey(Localidad, related_name='asentamientos', on_delete=models.CASCADE)
 
-    class Meta:
-        verbose_name = "Asentamiento"
-        verbose_name_plural = "Asentamientos"
-
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} - {self.localidad.nombre}"
