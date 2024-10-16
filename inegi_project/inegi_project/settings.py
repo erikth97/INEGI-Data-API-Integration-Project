df12dca6bd0fc577ef7bee2c0e5d1db81fe73a57
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-10-16 12:42:40 -0600

    feat: Configure pagination and optimization settings for DRF

--- inegi_project/inegi_project/settings.py
+++ inegi_project/inegi_project/settings.py
@@ -89,6 +89,15 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+}
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
