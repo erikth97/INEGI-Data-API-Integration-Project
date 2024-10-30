cc48166d1918705dbe35da61221ae7656c18217f
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-10-30 13:26:43 -0600

    feat: Add export data endpoints for CSV/Excel export functionality

--- inegi_project/api_inegi/urls.py
+++ inegi_project/api_inegi/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from .views import EstadoListView, MunicipioListView, LocalidadListView, AsentamientoListView
+from .views import (
+    EstadoListView, MunicipioListView, LocalidadListView, AsentamientoListView,
+)
+from .export_views import (
+    ExportEstadosView, ExportMunicipiosView, ExportLocalidadesView, ExportAsentamientosView
+)
 
 urlpatterns = [
     path('estados/', EstadoListView.as_view(), name='estados-list'),
     path('municipios/', MunicipioListView.as_view(), name='municipios-list'),
     path('localidades/', LocalidadListView.as_view(), name='localidades-list'),
     path('asentamientos/', AsentamientoListView.as_view(), name='asentamientos-list'),
+
+    # Rutas de exportación a CSV o Excel
+    path('export/estados/', ExportEstadosView.as_view(), name='export-estados'),
+    path('export/municipios/', ExportMunicipiosView.as_view(), name='export-municipios'),
+    path('export/localidades/', ExportLocalidadesView.as_view(), name='export-localidades'),
+    path('export/asentamientos/', ExportAsentamientosView.as_view(), name='export-asentamientos'),
 ]
